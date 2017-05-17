f8b6fdc64f836c0fe179415c608fcbbba4702273
Taras Rodynenko <t.rodynenko@gmail.com>
2017-05-17 12:35:43 +0300

    Initial - Cache and update

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const expressStylus = require('express-stylus-middleware');
+
+app.set('port', process.env.port || 3000);
+app.use('/css', expressStylus(__dirname + '/stylus-css/', { compress: true}));
+app.use(express.static('public'));
+app.set('views', "./views");
+app.set('view engine', 'pug');
+
+const routes = require('./routes');
+app.use(routes);
+
+app.listen(app.get('port'), () => {
+	console.log("Express server is running on port " + app.get('port'));
+});
---
+++ public/sw.js
@@ -0,0 +1,38 @@
+var CACHE = 'site-cache';
+
+self.addEventListener('install', function(event){
+  // Perform install steps
+  event.waitUntil(preCache());
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(fromCache(event.request));
+  event.waitUntil(update(event.request));
+});
+
+function preCache() {
+  return caches.open(CACHE).then(function (cache){
+    return cache.addAll([
+      '/',
+      '/i/img1.jpg',
+      '/i/img2.jpg',
+      '/i/img3.jpg',
+    ]);
+  });
+}
+
+function fromCache(request){
+  return caches.open(CACHE).then(function(cache){
+    return cache.match(request).then(function(matching){
+      return matching || Promise.reject('mo-match');
+    });
+  });
+}
+
+function update(request){
+  return caches.open(CACHE).then(function(cache){
+    return fetch(request).then(function(responce){
+      return cache.put(request, responce);
+    });
+  });
+}
